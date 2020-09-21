6bfa1601dcc68d9333f786a848037f824ded2ef8
Tomo Bessho <tomobessho018@gmail.com>
2020-09-21 08:04:39 -0500

    Revert "First Code Checkpoint"

--- 361Example/Models/Item.cs
+++ 361Example/Models/Item.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-
-public class Item
+﻿public class Item
 {
-    [Key]
     public int Id { get; set; }
     public string Name { get; set; }
-    public DateTime Date { get; set; }
-    public bool Checkoff { get; set; }
 }
