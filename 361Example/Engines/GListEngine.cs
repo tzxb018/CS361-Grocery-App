7a746d4c861e8d8775a45d93f558edb36083b850
Tomo Bessho <tomobessho018@gmail.com>
2020-10-04 11:22:59 -0500

    added in items accessor

---
+++ 361Example/Engines/GListEngine.cs
@@ -0,0 +1,45 @@
+﻿using _361Example.Models;
+using System;
+using System.Collections.Generic;
+
+namespace _361Example.Engines
+{
+    public class GListEngine : IGListEngine
+    {
+
+        public GList DeleteList(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetAllLists()
+        {
+            throw new NotImplementedException();
+        }
+
+        public GList GetList(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GList InsertList(GList gList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GList> SortLists()
+        {
+            throw new NotImplementedException();
+        }
+
+        public GList UpdateList(int id, GList gList)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<GList> IGListEngine.GetAllLists()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
