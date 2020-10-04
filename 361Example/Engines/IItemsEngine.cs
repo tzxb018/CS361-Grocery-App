6f7f4832d8621275f4a52aa9afcb6a07efa88e62
Tomo Bessho <tomobessho018@gmail.com>
2020-10-04 10:37:35 -0500

    merged from dev

---
+++ 361Example/Engines/IItemsEngine.cs
@@ -0,0 +1,20 @@
+﻿using _361Example.Models;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _361Example.Controllers
+{
+    public interface IGListEngine
+    {
+        IEnumerable<Item> GetAllItems();
+        Item GetItem(int id);
+        IEnumerable<GroceryList> SortLists();
+        Item InsertItem(Item item);
+        Item UpdateItem(int id, Item item);
+        Item DeleteItem(int id);
+    }
+}
