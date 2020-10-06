14b81155e9bf22f74bc4c54ed0e6e1a0ce0abf5e
Tomo Bessho <tomobessho018@gmail.com>
2020-10-06 10:29:49 -0500

    added in services for grocery list, need to implement codein services

--- 361Example/ClientApp/src/app/app.module.ts
+++ 361Example/ClientApp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -20,8 +20,10 @@ import { CreateNewAccComponent } from './CreateNewAcc/CreateNewAcc.component';
 import { ItemListComponent } from './item-list/item-list.component';
 import { FaqComponent } from './faq/faq.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent }
 
-
+];
 
 @NgModule({
   declarations: [
