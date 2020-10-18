7c93659b2e871a434a77c1aa27a44cb925724dfd
Tomo Bessho <tbessho2@unl.edu>
2020-10-18 12:58:29 -0500

    all tests pass with flying colors!

--- GroceryApp.Tests/AccessorTests/UserAccesorTests.cs
+++ GroceryApp.Tests/AccessorTests/UserAccesorTests.cs
@@ -1,11 +1,7 @@
 ﻿using _361Example.Accessors;
-using _361Example.Engines;
 using _361Example.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
 
 namespace GroceryApp.Tests
 {
@@ -35,6 +31,40 @@ namespace GroceryApp.Tests
             Assert.AreEqual(expectedOne, one, "DB was analyzed incorrectly");
             Assert.AreEqual(expectedTwo, two, "DB was analyzed incorrectly");
             Assert.AreEqual(expectedFive, five, "DB was analyzed incorrectly");
+        }
+
+        [TestMethod]
+        public void UserAccessor_GetUser()
+        {
+            // Arrange: setting the objects of all the expected users in the database (the database is seeded with data from SQL script)
+            var expected1 = new User { Id = 1, email = "johnsmith@gmail.com", password = "dlka3jgd45" };
+            var expected2 = new User { Id = 2, email = "johndoe@hotmail.com", password = "sdjdsbf" };
+            var expected3 = new User { Id = 3, email = "helloworld@cse.edu", password = "jfioseufho" };
+
+            // Act: get the users from the database by their IDs
+            var result1 = userAccessor.Find(1);
+            var result2 = userAccessor.Find(2);
+            var result3 = userAccessor.Find(3);
+
+            // Assert: using their user names and passwords, we check to see if the database returned the expected results
+            Assert.AreEqual(expected1, result1, expected1.Id + " " + expected1.email + " was expected. " + result1.Id + " " + result1.email + " was returned.");
+            Assert.AreEqual(expected2, result2, expected2.Id + " " + expected2.email + " was expected. " + result2.Id + " " + result2.email + " was returned.");
+            Assert.AreEqual(expected3, result3, expected3.Id + " " + expected3.email + " was expected. " + result3.Id + " " + result3.email + " was returned.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void GListEngine_GetList_ElementDoesntExist()
+        {
+            // Arrange: The expected result should be null, since there is no user with ID = 10
+
+
+            // Act: Calls the userAccessor Find() method on a item that doesn't exist
+            var result = userAccessor.Find(10);
+
+            // Assert checks if the result is null
+            Assert.AreEqual(null, result, result.Id + " was returned. ");
+
         }
 
 
