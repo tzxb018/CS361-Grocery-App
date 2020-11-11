48dd0c798000bd5146f66a2e90f303bb4b41663a
Bryce Jen Wei Yong <54676043+bryceyong@users.noreply.github.com>
2020-11-11 11:50:38 -0600

    Succesfully posted Account api

--- 361Example/ClientApp/src/app/home/home.component.ts
+++ 361Example/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Injectable, Output, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoginService } from '../login.service';
+import { DataService } from '../data.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
+
+@Injectable({
+  providedIn: 'root'
+})
 export class HomeComponent {
+
+  // holds all the glists for the component
+  public allUsers: User[];
+
+  
+
+
+
+
 }
+
+interface User {
+  id: number;
+  email: string;
+  password: string;
+}
+
+
---
+++ 361Example/ClientApp/src/app/login.service.ts
@@ -0,0 +1,60 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, retry } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private headers: HttpHeaders;
+
+  // public list to hold all the glist instances
+  public users: User[];
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+    this.headers = new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' });
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+
+  public getAllUser() {
+
+    // this will only populate the user if there are no errors
+    return this.http.get<User[]>(this.baseUrl + 'user')
+      .pipe(
+        retry(3),
+        catchError(this.handleError)
+      );
+  }
+
+  public update(payload) {
+    return this.http.put(this.baseUrl + '/' + payload.id, payload, { headers: this.headers });
+  }
+}
+interface User {
+  id: number;
+  email: string;
+  password: string;
+}
