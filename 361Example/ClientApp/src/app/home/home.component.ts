adb12e7a425f02cc8ef4c41eea192e960b880f3a
Bryce Jen Wei Yong <54676043+bryceyong@users.noreply.github.com>
2020-11-13 12:58:43 -0600

    Implemented login verification

--- 361Example/ClientApp/src/app/home/home.component.ts
+++ 361Example/ClientApp/src/app/home/home.component.ts
@@ -16,13 +16,41 @@ export class HomeComponent {
 
   // holds all the glists for the component
   public allUsers: User[];
-
+  public login: number;
   
 
+  constructor(private loginService: LoginService,) {
+    this.refreshTable();
+  }
 
+  //func to refresh table
+  async refreshTable() {
 
+    const result = await this.loginService.getAllUser().toPromise();
+    this.allUsers = result;
 
-}
+  }
+
+  verifyUser() {
+    const emailForm = document.getElementById("email") as HTMLInputElement;
+    const email = emailForm.value;
+    const passwordForm = document.getElementById("password") as HTMLInputElement;
+    const password = passwordForm.value;
+    
+    if (email.length > 0) {
+      for (const user of this.allUsers) {
+        if (email == user.email && password == user.password) {
+          this.login = 1;
+          break;
+        } else {
+          this.login = 0;
+        }
+      }
+    }
+    
+  }
+  
+ }
 
 interface User {
   id: number;
